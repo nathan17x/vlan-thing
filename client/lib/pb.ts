1b180a7573c01d4494660cc26d4f06f71e9cdbaa
Nathan Mahoney <nathan17x@gmail.com>
2025-11-12 18:01:46 -0500

    compose file changes

--- client/lib/pb.ts
+++ client/lib/pb.ts
@@ -1,5 +1,4 @@
 import PocketBase from "pocketbase";
-console.log('okkokokok')
 console.log(process.env.NEXT_PUBLIC_PB_URL)
 export const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
 pb.autoCancellation(false)
